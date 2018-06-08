157a75509f166c24a20f694a9c7bb457568e719e
xuedesign <elmo@xuedesign.tw>
2018-06-08 18:16:19 +0800

    首頁製作 30% #20180608

--- js/main.js
+++ js/main.js
@@ -1,17 +1,38 @@
+//進行設定
 require.config({
+    
+    // 定義路徑別名
     paths: {
         //jQuery總碼
         "jquery": "https://code.jquery.com/jquery.min",
         //漢字標準格式 https://css.hanzi.co/manual/
 　　　　　"hanzi": "https://cdnjs.cloudflare.com/ajax/libs/Han/3.3.0/han.min",
 　　　　　//圖片延遲載入
-        "lazyload": "https://cdn.jsdelivr.net/npm/lazyload@2.0.0-beta.2/lazyload.min",
+        "lazyload": "https://cdn.jsdelivr.net/npm/lazyload@1.8.4/jquery.lazyload.min",
         //物件載入動畫
-        "wow": "lib/wow.min",
+        "wow": "https://cdn.rawgit.com/graingert/WOW/34712a3d/dist/wow.min"},
+    
+    // 配置依賴性
+    shim: {
+        'lazyload' : {
+            deps: ['jquery']
+        },
     }
+    
 });
 
-require(['jquery', 'hanzi','lazyload', 'wow'], function ($, hanzi, lazyload, wow){
+require([
+        'jquery', 
+        'hanzi', 
+        'lazyload', 
+        'wow'], 
+    function (
+        $, 
+        hanzi,
+        wow, 
+        lazyload, 
+        ){
+    
     // 另開視窗
     function externalLinks() { 
      if (!document.getElementsByTagName) return; 
@@ -29,6 +50,7 @@ require(['jquery', 'hanzi','lazyload', 'wow'], function ($, hanzi, lazyload, wow
     $("img.lazyload").lazyload();
 
     //wow動畫
-    wow=new WOW({animateClass:"animated",offset:100,callback:function(a){console.log("WOW: animating <"+a.tagName.toLowerCase()+">")}}),wow.init();
+    new WOW().init();
+    //wow=new WOW({animateClass:"animated",offset:100,callback:function(a){console.log("WOW: animating <"+a.tagName.toLowerCase()+">")}}),wow.init();
 　　
 });
