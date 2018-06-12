1ee841b42ca2c3c73881af5f9a16017509c89555
xuedesign <elmo@xuedesign.tw>
2018-06-12 18:08:23 +0800

    關於學會製作完成 #20180612

---
+++ script/_page.js
@@ -0,0 +1,34 @@
+$LAB
+
+//jQuery總碼
+.script("https://code.jquery.com/jquery.min.js")
+
+//圖片延遲載入
+.script("https://cdn.jsdelivr.net/npm/lazyload@1.8.4/jquery.lazyload.min.js").wait(function(){
+    $("img.lazyload").lazyload();
+})
+
+//區塊載入動畫
+.script("https://cdn.rawgit.com/graingert/WOW/34712a3d/dist/wow.min.js").wait(function(){
+    wow=new WOW({animateClass:"animated",offset:100,callback:function(a){console.log("WOW: animating <"+a.tagName.toLowerCase()+">")}}),wow.init();
+})
+
+//totop
+.script("/script/lib/jquery.ui.totop.js")
+.script("/script/lib/easing.js").wait(function(){
+    $(document).ready(function(){$().UItoTop({easingType:"easeOutQuart"})}); 
+})
+
+
+// 另開視窗
+function externalLinks() { 
+ if (!document.getElementsByTagName) return; 
+ var anchors = document.getElementsByTagName("a"); 
+ for (var i=0; i<anchors.length; i++) { 
+   var anchor = anchors[i]; 
+   if (anchor.getAttribute("href") && 
+       anchor.getAttribute("rel") == "external") 
+     anchor.target = "_blank"; 
+ } 
+} 
+window.onload = externalLinks;
